09a9ded0772519d5e5459469684ea51edbb8c7fe
Alisher Nurmatov <118418648+AliNur17@users.noreply.github.com>
2026-03-10 16:02:57 -0700

    Update Main.java
    Alisher

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Game Start!");
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Game game = new Game();
+        game.play();
+        // Add reset -- replay capability
     }
 }
