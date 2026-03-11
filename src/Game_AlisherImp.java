11ccfbb7ff016f2b8b36daec79cc3b548fcec6d5
Alisher Nurmatov <118418648+AliNur17@users.noreply.github.com>
2026-03-10 18:36:37 -0700

    Update Game_AlisherImp.java

--- src/Game_AlisherImp.java
+++ src/Game_AlisherImp.java
@@ -1,33 +1,95 @@
-import java.util.Scanner;
-
 public class Game
 {
     private int roundCounter;
+    private final int totalNumberRounds;
+    private int player1Wins, player2Wins, drawGames = 0;
+    private boolean checkSamePlayerType;
+
     RuleEngine engine = new RuleEngine();
-    Player human = PlayerFactory.createPlayer("Human", "You");
-    Player computer = PlayerFactory.createPlayer("AI", "Computer");
+    private final String player1Type;
+    private final String player2Type;
+    Player p1, p2;
+
 
     public Game()
     {
-        this.roundCounter = 1;
+        roundCounter = 1;
+        totalNumberRounds = 20;
+
+        player1Type = "Human";
+        player2Type = "AI";
+
+        p1 = PlayerFactory.createPlayer(player1Type, "Human");
+        p2 = PlayerFactory.createPlayer(player2Type, "Computer");
     }
 
     private void nextRound()
     {
-        this.roundCounter++;
+        roundCounter++;
+    }
+
+    private void updateScore(int winnerVal, String p1Name, String p2Name)
+    {
+        switch(winnerVal)
+        {
+            case(0):
+                drawGames++;
+                System.out.println("Draw!");
+                break;
+            case(1):
+                player1Wins++;
+                System.out.println(p1Name + " wins!");
+                break;
+            case(2):
+                player2Wins++;
+                System.out.println(p2Name+" wins!");
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void showCurrentScore()
+    {
+        System.out.println("Score: " + p1.getName() + ":" + player1Wins + " " + p2.getName() + ":" + player2Wins + " Draws=" + drawGames + "\n");
+    }
+
+    private void checkHidden()
+    {
+        checkSamePlayerType = player1Type.equals(player2Type);
     }
 
     public void play()
     {
-        Scanner input = new Scanner(System.in);
+        System.out.println("\n");
+        String player1Name = p1.getName();
+        String player2Name = p2.getName();
 
-        while (this.roundCounter <= 20)
+        while (roundCounter <= totalNumberRounds)
         {
-            System.out.print("Round " + this.roundCounter + " - Choose " + engine.availableChoices() + ": ");
-            int choice = input.nextInt();
-            System.out.println("You chose >> "+engine.selection(choice));
+            System.out.print("Round " + roundCounter + " - Choose " + engine.availableChoices() + ": ");
+            String p1Decision, p2Decision;
+            if (checkSamePlayerType) //In case of two human or non-human players
+            {
+                p1.updateHidden(checkSamePlayerType);
+                p1Decision = p1.makeMove();
+                p2Decision = p2.makeMove();
+                //To differentiate the two players without repeating a descriptor like "Human" or "You" as an example
+                player1Name = "Player 1";
+                player2Name = "Player 2";
+                System.out.print(player1Name + " chose: " + p1Decision + ". " + player2Name + " chose: " + p2Decision);
+            }
+            else //Different player types
+            {
+                p1Decision = p1.makeMove();
+                p2Decision = p2.makeMove();
+            }
+            int winner = engine.getVictor(p1Decision, p2Decision);
+            updateScore(winner, player1Name, player2Name);
+            showCurrentScore();
             nextRound();
         }
-        System.out.println("Game over");
+        System.out.println("\nGame Over. The final scores are as follows...");
+        showCurrentScore();
     }
 }
