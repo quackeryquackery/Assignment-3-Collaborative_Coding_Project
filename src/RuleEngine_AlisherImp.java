8b16f1df99598c4082ce31e4cab6fc21180ec175
Alisher Nurmatov <118418648+AliNur17@users.noreply.github.com>
2026-03-10 18:37:07 -0700

    Update RuleEngine_AlisherImp.java

--- src/RuleEngine_AlisherImp.java
+++ src/RuleEngine_AlisherImp.java
@@ -4,6 +4,11 @@ import java.util.Map;
 public class RuleEngine
 {
     private static final LinkedHashMap<Integer, String> possibleChoices = new LinkedHashMap<>();
+    static String[][] rules = {
+            {"rock", "scissors"}, //rock beats scissors
+            {"scissors", "paper"}, //scissors beats paper
+            {"paper", "rock"} //paper beats rock
+    };
 
     public RuleEngine()
     {
@@ -20,23 +25,33 @@ public class RuleEngine
         {
             output = output.concat(key+"="+possibleChoices.get(key)+", ");
         }
-        output = output.substring(0, output.length() - 2); // Remove trailing comma
+        output = output.substring(0, output.length() - 2); //Remove trailing comma
         output = output.concat(")");
-        return output;
-
-        // Currently returns >> (1=Rock, 2=Paper, 3=Scissors)
+        return output; //Currently returns >> (1=Rock, 2=Paper, 3=Scissors)
     }
 
-    public String selection(int choice)
+    public static String selection(int choice)
     {
-        return possibleChoices.get(choice);
-        // If user inputs 1, returns Rock, and so on...
+        return possibleChoices.get(choice); //If user inputs 1, returns Rock, and so on...
     }
 
+    public int getVictor(String p1Choice, String p2Choice) {
+        if (p1Choice.equalsIgnoreCase(p2Choice)) {
+            return 0; //draw
+        }
 
+        for (String[] rule : rules) {
+            String winnerMove = rule[0]; //What beats the other choice (left column of rules array)
+            String loserMove = rule[1]; //What gets beaten by the other choice (right column of rules array)
 
-    public void getVictor(String firstPlayer, String secondPlayer)
-    {
+            if (p1Choice.equalsIgnoreCase(winnerMove) && p2Choice.equalsIgnoreCase(loserMove)) {
+                return 1; //player 1 wins
+            }
+            if (p2Choice.equalsIgnoreCase(winnerMove) && p1Choice.equalsIgnoreCase(loserMove)) {
+                return 2; //player 2 wins
+            }
+        }
 
+        return -1; // invalid input
     }
 }
